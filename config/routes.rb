711ebf114e2e271b40c40127287cbaaa601b16e8
Vandalisk <zakharuni13@gmail.com>
2015-02-16 13:32:37 +0400

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
-SampleApp::Application.routes.draw do 
-  get "users/new"
-  
-  match '/signup',  to: 'users#new'
+SampleApp::Application.routes.draw do
 
-  root to: 'static_pages#home' 
+  root to: 'static_pages#home'
+
+  resources :users 
+  
+  match '/signup',  to: 'users#new'   
 
   match '/help', to: 'static_pages#help'
 
   match '/about', to: 'static_pages#about'
 
-  match '/contact', to: 'static_pages#contact'
+  match '/contact', to: 'static_pages#contact'  
 
   # get '/home', to: 'static_pages#home', as: 'home'
 
