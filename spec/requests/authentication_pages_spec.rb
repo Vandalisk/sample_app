3863a2f3557ed733549774d94da2cb71ae1461ae
Vandalisk <zakharuni13@gmail.com>
2015-02-17 14:18:46 +0400

    Before autorization

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -28,11 +28,7 @@ describe "Authentication" do
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-      before do
-        fill_in "Email",    with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button "Sign in"
-      end
+      before { sign_in user } 
 
      describe "followed by signout" do
         before { click_link "Sign out" }
